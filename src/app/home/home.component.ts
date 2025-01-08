853aead2bdfdc872f9a6761c503f20efd26736ba
Ravi Kant Prasad <ravikantp@byteridge.com>
2025-01-08 13:59:38 +0530

    Data binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CounterComponent } from '../components/counter/counter.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CounterComponent, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  counter = signal(5);
+  value = 10;
+  message = 'Hello World!';
+  inputValue = '';
 }
