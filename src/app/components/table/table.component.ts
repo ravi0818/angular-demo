e3cd16f7e839576c07088a85f23264c79669de87
Ravi Kant Prasad <ravikantp@byteridge.com>
2025-01-13 12:36:37 +0530

    Reusable table

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,54 @@
+import {
+  Component,
+  effect,
+  ViewChild,
+  input,
+  TemplateRef,
+} from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-table',
+  imports: [
+    MatTableModule,
+    MatSortModule,
+    MatPaginator,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    CommonModule,
+  ],
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+})
+export class TableComponent {
+  tableData = input.required<any>();
+  columns = input.required<string[]>();
+  isLoading = input.required<boolean>();
+  customCellTemplates = input.required<Record<string, TemplateRef<any>>>();
+  customCellRenderer!: Record<string, TemplateRef<any>>;
+
+  displayedColumns: string[] = [];
+  dataSource = new MatTableDataSource<unknown>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor() {
+    effect(() => {
+      const data = this.tableData();
+      this.dataSource.data = data;
+      this.displayedColumns = this.columns();
+    });
+    effect(() => {
+      this.customCellRenderer = this.customCellTemplates();
+    });
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+}
