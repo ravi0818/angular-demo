632e57e9974a70d2748c0bcd469b799a99ca203a
Ravi Kant Prasad <ravikantp@byteridge.com>
2025-01-22 10:12:36 +0530

    delete confirmation added

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -4,6 +4,8 @@ import { Todo } from '@interface/todo';
 import { TodoService } from '@services/todo.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '@components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-todo-item',
@@ -17,12 +19,23 @@ export class TodoItemComponent {
 
   onEditClick = output<Todo>();
 
+  dialog = inject(MatDialog);
+
   toggleTodo = (todo: Todo) => {
     this.todoService.updateTodo({ ...todo, done: !todo.done });
   };
 
   deleteTodo = (todo: Todo) => {
-    this.todoService.deleteTodo(todo);
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: 'Confirm',
+        message: 'Are you sure you want to delete this item?',
+      },
+    });
+
+    dialogRef.componentInstance.onConfirm.subscribe(() => {
+      this.todoService.deleteTodo(todo);
+    });
   };
 
   editTodo = (todo: Todo) => {
